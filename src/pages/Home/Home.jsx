adbad34b144403b76deec252a6caadea864f11d5
Johnathan Hill <jhilladaybeats@gmail.com>
2024-07-01 15:56:27 -0700

    Refactors and adds back Home Components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Hero from "./components/Hero/Hero";
+import WeekSpecials from "./components/WeekSpecials/WeekSpecials";
+import Testimonials from "./components/Testimonials/Testimonials";
+import OurStory from './components/OurStory/OurStory'
 
 function Home() {
   return (
     <>
-
+      <Hero />
+      <WeekSpecials />
+      <Testimonials />
+      <OurStory />
     </>
   );
 }
