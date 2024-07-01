6c07cab9827900a5780c364c60528ba200d78560
Johnathan Hill <jhilladaybeats@gmail.com>
2024-07-01 15:40:59 -0700

    Adds route for pages not found, scroll to top functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from './components/Layout/Layout.jsx'
 import Home from './pages/Home/Home.jsx';
+import NotFound from './pages/Not Found/NotFound.jsx';
 import Reservations from './pages/Reservations/Reservations.jsx';
 import ConfirmedReservation from './pages/Reservations/confirmedBookings.jsx';
 
@@ -16,6 +17,7 @@ const App = () => {
             path="/confirmedReservation"
             element={<ConfirmedReservation />}
           />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </>
